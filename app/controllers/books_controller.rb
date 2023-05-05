413b4f952148c600ed65d995c80f142771f11c5f
PB-193 <bigbamboopbo@yahoo.co.jp>
2023-05-05 14:16:23 +0000

    destroyCreate

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,4 +12,38 @@ class BooksController < ApplicationController
     # @book.year = 2019 
     # で直接フォームに入力している状態にできる
   end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to books_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:year,:month, :inout, :amount, :category)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
     get "/books" => "books#index"
+    post "/books" => "books#create"
     get "/books/new" => "books#new" , as:"new_book"
+    get "/books/:id/edit" => "books#edit" , as:"edit_book"
     get "/books/:id" => "books#show" , as:"book"
+    
+    patch "/books/:id" => "books#update" 
+    # ニックネームを書いていないですが書いていない場合は、すぐ上のニックネーム(book)を引き継ぎます。
+    # showと同じニックネーム・URLパターンになってしまいますが、HTTPメソッドが異なるのでちゃんと区別されます。
+    delete "/books/:id" => "books#destroy"
 end
